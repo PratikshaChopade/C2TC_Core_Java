1ba934eb02f08f3c838cebcfb41974c58bd3e78a
q <q@LAPTOP-0H2DSNPH>
2022-06-15 11:56:11 +0530

    first m11 commit

---
+++ HalloWorld/src/hallo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author q
+ *
+ */
+public class hallo {
+
+	/**
+	 * 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HalloWorld");
+		// TODO Auto-generated method stub
+
+	}
+
+}
