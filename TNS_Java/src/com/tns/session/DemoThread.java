64bc50eee766f8261bbcbc6f767c1539f0f05513
q <q@LAPTOP-0H2DSNPH>
2022-07-23 20:23:16 +0530

    first commit

--- TNS_Java/src/com/tns/session/DemoThread.java
+++ TNS_Java/src/com/tns/session/DemoThread.java
@@ -1,10 +1,30 @@
 package com.tns.session;
 
-public class DemoThread {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		class MyThread1 implements Runnable
+		{
+			public void run()
+			{
+				for(int i=0;i<5;i++)
+				{
+							System.out.println(i);
+					
+				 }
+		        }
+				}
 
+
+
+		   public class DemoThread
+		{
+
+			public static void main(String[] args) 
+			{
+			MyThread1 t=new MyThread1();
+			Thread s=new Thread(t);
+			s.start(); // Start your Thread
+		}
+		
 	}
 
-}
+
