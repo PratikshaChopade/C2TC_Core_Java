c660394b3eec9bfa2ba81cbb49277cafced6f91b
q <q@LAPTOP-0H2DSNPH>
2022-07-23 19:25:41 +0530

    core java

---
+++ TNS/src/com/tns/session/Array3.java
@@ -0,0 +1,15 @@
+package com.tns.session;
+
+public class Array3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a= {1,2,3,4,5};
+		
+		
+		
+		for(int i=0;i<5;i++)
+		System.out.println(a[i]);
+	}
+
+}
