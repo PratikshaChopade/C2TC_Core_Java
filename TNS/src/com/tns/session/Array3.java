5f30b5731bc01f9cf03a4b1e33f6f1e84bec37c9
q <q@LAPTOP-0H2DSNPH>
2022-07-23 19:33:15 +0530

    first commit

--- TNS/src/com/tns/session/Array3.java
+++ TNS/src/com/tns/session/Array3.java
@@ -4,12 +4,7 @@ public class Array3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a= {1,2,3,4,5};
-		
-		
-		
-		for(int i=0;i<5;i++)
-		System.out.println(a[i]);
+
 	}
 
 }
