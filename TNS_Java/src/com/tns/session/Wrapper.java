d60873a61fda55fff201ff902ca765a54c9c8368
q <q@LAPTOP-0H2DSNPH>
2022-08-10 18:13:14 +0530

    first commit

--- TNS_Java/src/com/tns/session/Wrapper.java
+++ TNS_Java/src/com/tns/session/Wrapper.java
@@ -4,7 +4,12 @@ public class Wrapper {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Boolean b=new Boolean("True");
+		Boolean c=new Boolean("NO");
+		
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(b.equals(c));
 	}
 
 }
